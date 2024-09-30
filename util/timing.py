e5dd41cfacd743d9a6da630326e4b87c5c3a4798
Pan <chaopan@amazon.com>
2024-09-30 14:48:17 +0800

    add 3tb ddl

---
+++ util/timing.py
@@ -0,0 +1,13 @@
+import time
+from functools import wraps
+
+def timing_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        duration = end_time - start_time
+        print(f"{func.__name__} 执行时间: {duration:.4f} 秒")
+        return result
+    return wrapper
